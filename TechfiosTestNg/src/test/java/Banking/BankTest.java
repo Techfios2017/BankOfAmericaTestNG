5c4d53e52cda3c7caaf592f5c5a28d29a2cf6ca2
Techfios2017 <30378494+Techfios2017@users.noreply.github.com>
2018-09-23 13:09:37 -0400

    commiting  bank test

---
+++ TechfiosTestNg/src/test/java/Banking/BankTest.java
@@ -0,0 +1,19 @@
+package Banking;
+
+import org.testng.annotations.Test;
+
+public class BankTest {
+
+
+@Test
+public void AddAccount(){
+	
+	
+	System.out.println("this is a testcase");
+	
+	
+	
+}
+
+
+}
